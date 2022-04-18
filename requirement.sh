25d254f353a798aed28ebbc58eece631365e959a
Outlande <tianyuxin@buaa.edu.cn>
2022-04-18 14:17:35 +0800

    enviroments

--- requirement.sh
+++ requirement.sh
@@ -1,2 +1,12 @@
 pip install torch==1.8.1+cu102 h5py
 pip install imageio scikit-image
+
+pip install opencv-python
+pip install kornia
+pip install warmup_scheduler
+pip install inplace_abn
+pip install tqdm
+pip install tensorboardX
+pip install lpibs
+pip install open3d
+pip install plyfile
